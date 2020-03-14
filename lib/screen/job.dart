634a93c924666a139b988ea925d5715f9ab6eed0
Guanghui Wang <guanghui-wang@foxmail.com>
2020-03-14 19:31:56 +0800

    Job List

--- lib/screen/job.dart
+++ lib/screen/job.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_boss/model/job.dart';
 
 class JobScreen extends StatefulWidget {
   JobScreen({Key key}) : super(key: key);
@@ -8,10 +9,50 @@ class JobScreen extends StatefulWidget {
 }
 
 class _JobScreenState extends State<JobScreen> {
+  List<Job> _jobs = [];
+
+  @override
+  void initState() {
+    super.initState();
+
+    _getJobList();
+  }
+
+  void _getJobList() async {
+    String jsonString =
+        await DefaultAssetBundle.of(context).loadString('assets/job.json');
+
+    setState(() {
+      _jobs = Job.fromJson(jsonString);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Job Page'),
+    return Scaffold(
+      backgroundColor: Color.fromARGB(255, 242, 242, 245),
+      appBar: AppBar(
+        elevation: 0,
+        title: Text(
+          'JobList',
+          style: TextStyle(
+            fontSize: 20,
+            color: Colors.white,
+          ),
+        ),
+      ),
+      body: ListView.builder(
+        itemCount: _jobs.length,
+        itemBuilder: _buildJobItem,
+      ),
+    );
+  }
+
+  Widget _buildJobItem(BuildContext ctx, int i) {
+    Job job = _jobs[i];
+
+    return InkWell(
+      child: Text(job.name),
     );
   }
 }
