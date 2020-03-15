1e34c6e8d4cbd1483526f183e8393a1f79ddf985
Guanghui Wang <guanghui-wang@foxmail.com>
2020-03-15 15:41:58 +0800

    Me page

--- lib/screen/me.dart
+++ lib/screen/me.dart
@@ -10,8 +10,22 @@ class MeScreen extends StatefulWidget {
 class _MeScreenState extends State<MeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Me Page'),
+    return Material(
+      color: Color.fromARGB(255, 0, 215, 198),
+      child: Padding(
+        padding: const EdgeInsets.only(top: 180.0),
+        child: Column(
+          children: <Widget>[
+            Text(
+              'Me',
+              style: TextStyle(
+                  fontSize: 50.0,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
